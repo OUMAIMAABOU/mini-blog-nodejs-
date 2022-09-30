6d1ab13a87528f5f686bf60d36a82e54d09f78cf
oumaima <oumaimaabouelhaitam@gmail.com>
2022-09-30 08:39:44 +0100

    update structeur

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
 const express = require("express");
 
 const app = express();
-
-// parse requests of content-type - application/json
 app.use(express.json());
-
-// parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
-// simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to bezkoder application." });
 });
 
 
-// set port, listen for requests
 
 
 const db = require("./app/models");
