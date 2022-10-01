b82d79fc25ef4fff0b8ede2bb48e650468a99251
Boubker Ennaqar <93933750+EnnaqarBoubker@users.noreply.github.com>
2022-10-01 20:07:16 +0100

    added two function in avis controller file create && get all items

--- app/controllers/avis.controller.js
+++ app/controllers/avis.controller.js
@@ -1,5 +1,6 @@
+const { avis } = require('../models');
 const db = require('../models');
-const avis = db.avis;
+const Avis = db.avis;
 const Op = db.Sequelize.Op;
 
 // Create and Save a new Tutorial
@@ -12,7 +13,7 @@ exports.create = (req, res) => {
     };
 
     // save avis in data base
-    avis.create(avis)
+    Avis.create(avis)
         .then(data => { res.send(data) })
         .catch(err => {
             res.status(500).send({
@@ -20,4 +21,34 @@ exports.create = (req, res) => {
                     err.message || "Some error occurred while creating the Avis."
             })
         })
+};
+
+exports.show = (req, res) => {
+    console.log('hi admiral')
+};
+
+exports.findAll = (req, res) => {
+    const name = req.query.name;
+    let condition = name ? {name : {[Op.like] : `%${name}%`}} : null;
+
+    avis.findAll({where : condition})
+    .then( data => {res.send(data)})
+    .catch(err => {
+        res.status(500).send({
+            message : 
+            err.message || "some error occurred while retrieving avis"
+        })
+    })
+};
+
+exports.findOne = (req, res) => {
+
+};
+
+exports.update = (req, res) => {
+
+};
+
+exports.delete = (req, res) => {
+
 };
--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,22 +1,24 @@
 module.exports = app => {
-  // const commentaire = require("../controllers/commentaire.controller.js");
-  const avis = require('../controllers/avis.controller.js')
+  const commentaire = require("../controllers/commentaire.controller.js");
+  const avis = require('../controllers/avis.controller')
 
   var router = require("express").Router();
 
-  // router.post("/", commentaire.create);
-  router.post("/", avis.create);
+  router.post("/", commentaire.create);
+  router.post("/createAvis", avis.create);
 
-  // router.get("/", commentaire.findAll);
-  
-  
-  // router.get("/:id", commentaire.findOne);
-
-  // router.put("/:id", commentaire.update);
-
-  // router.delete("/:id", commentaire.delete);
+  router.get("/", commentaire.findAll);
+  router.get("/showAvis", avis.findAll);
 
 
-  // app.use('/commentaire', router);
+  router.get("/:id", commentaire.findOne);
+  router.get("/gitAvis/:id", avis.findOne);
+
+  router.put("/:id", commentaire.update);
+
+  router.delete("/:id", commentaire.delete);
+
+
+  app.use('/commentaire', router);
   app.use('/avis', router);
 };
