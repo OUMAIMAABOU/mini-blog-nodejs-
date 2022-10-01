f3b4f1e524c2e08a62e52b2813d9d0c5ae8a90b8
oumaima <oumaimaabouelhaitam@gmail.com>
2022-10-01 21:32:24 +0100

    instal ejs

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.set('views','./app/views')
+app.set('view engine','ejs')
 
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to  application." });
+  res.render('home');
 });
 
 
