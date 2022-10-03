2dbcf0afbe8552f141b96f13f2eb33997a978a93
khalid hagane <khalidhagane62@gmail.com>
2022-10-03 11:05:48 +0100

    terminer crud

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ db.sequelize.sync()
 
 
   require("./app/routes/routes")(app);
-  const port=process.env.PORT || 3000
+  const port=process.env.PORT || 8080
   console.log('The value of PORT is:', process.env.PORT ,port);
 
   app.listen(port)
