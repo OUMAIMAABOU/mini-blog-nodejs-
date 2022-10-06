33e99ba1c731e5e13487b816840c8d2c3b68207e
Nouamane <nouamanlamkadmxd@gmail.com>
2022-10-06 16:35:23 +0100

    fix things

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const express = require("express");
-const dashboard = express();
-dashboard.use(express.json());
-const homePage = express();
-homePage.use(express.json());
-dashboard.use(express.urlencoded({ extended: true }));
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 require('dotenv').config();
-dashboard.use(express.static('public'))
-homePage.use(express.static(__dirname + '/views/homePage'));
-const { findAllcommentaire,findOnecommentaire}   = require('./app/controllers/commentaire.controller.js')
+app.use(express.static('public'))
+app.use(express.static(__dirname + '/views'));
+const { findAllcommentaire}   = require('./app/controllers/commentaire.controller.js')
 
 const db = require("./app/models");
 
@@ -20,48 +18,44 @@ db.sequelize.sync()
     console.log("db not connected " + err.message);
   });
 
-dashboard.set('views','./views/dashboard')
-dashboard.set('view engine','ejs')
-homePage.set('views','./views/homePage')
-homePage.set('view engine','ejs')
+app.set('views','./views')
+app.set('view engine','ejs')
 
 
 
 // ________________________ homePage ______________________
 
-homePage.get("/", (req, res) => {
+app.get("/", (req, res) => {
   res.render('homePage');
 });
-homePage.get("/blog", (req, res) => {
+app.get("/blog", (req, res) => {
   res.render('blog_details');
 });
 // ________________________ dashboard ______________________
 // Dashboard
-dashboard.get("/", (req, res) => {
+app.get("/", (req, res) => {
   res.render('dashboard');
 });
-dashboard.get('/dash', (req, res) => {
+app.get('/dash', (req, res) => {
   res.render('dashboard')
 })
-dashboard.get('/settings', (req, res) => {
+app.get('/settings', (req, res) => {
   res.render('settings')
 })
 // Articles
-dashboard.get('/articles', async(req, res) => {
-  let commantaire = await findAllcommentaire()
-  res.render('articles', {commantaire})
+app.get('/articles', async(req, res) => {
+  res.render('articles')
 })
 // Categories
-dashboard.get('/categories', async(req, res) => {
-  let commantaire = await findAllcommentaire()
-  res.render('categories', {commantaire})
+app.get('/categories', async(req, res) => {
+  res.render('categories')
 })
 // Comments
-dashboard.get('/comments', async(req, res) => {
+app.get('/comments', async(req, res) => {
   let commantaire = await findAllcommentaire()
   res.render('avisComme', {commantaire})
 })
-dashboard.get('/commantaire/edite/:id', async(req, res) => {
+app.get('/commantaire/edite/:id', async(req, res) => {
   let commantaire = await findAllcommentaire()
   res.render('updatecomment', {commantaire})
 })
@@ -72,10 +66,9 @@ dashboard.get('/commantaire/edite/:id', async(req, res) => {
 
 
 
-require("./app/routes/routes")(homePage);
+require("./app/routes/routes")(app);
 const port = process.env.PORT || 8080
 console.log('The value of PORT is:', process.env.PORT ,port);
 
-dashboard.listen(port)
-homePage.listen(3040)
+app.listen(port)
 
